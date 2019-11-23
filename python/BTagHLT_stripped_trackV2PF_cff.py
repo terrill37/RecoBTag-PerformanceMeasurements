44746d23d1dd73c1d7b24e59021f6c2e5ad2a1fc
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2019-11-23 16:55:45 +0100

    add different tracking versions

---
+++ python/BTagHLT_stripped_trackV2PF_cff.py
@@ -0,0 +1,374 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+import copy
+from pdb import set_trace
+
+
+hltVerticesPF = cms.EDProducer("PrimaryVertexProducer",
+    TkClusParameters = cms.PSet(
+        TkDAClusParameters = cms.PSet(
+            Tmin = cms.double(2.4),
+            Tpurge = cms.double(2.0),
+            Tstop = cms.double(0.5),
+            coolingFactor = cms.double(0.6),
+            d0CutOff = cms.double(999.0),
+            dzCutOff = cms.double(4.0),
+            uniquetrkweight = cms.double(0.9),
+            use_vdt = cms.untracked.bool(True),
+            vertexSize = cms.double(0.15),
+            zmerge = cms.double(0.01)
+        ),
+        algorithm = cms.string('DA_vect')
+    ),
+    TkFilterParameters = cms.PSet(
+        algorithm = cms.string('filter'),
+        maxD0Significance = cms.double(999.0),
+        maxEta = cms.double(100.0),
+        maxNormalizedChi2 = cms.double(20.0),
+        minPixelLayersWithHits = cms.int32(2),
+        minPt = cms.double(0.0),
+        minSiliconLayersWithHits = cms.int32(5),
+        trackQuality = cms.string('any')
+    ),
+    # TrackLabel = cms.InputTag("hltPFMuonMerging"),
+    # TrackLabel = cms.InputTag("earlyGeneralTracks"),#v1
+    TrackLabel = cms.InputTag("generalTracks"),
+    beamSpotLabel = cms.InputTag("hltOnlineBeamSpot"),
+    verbose = cms.untracked.bool(False),
+    vertexCollections = cms.VPSet(
+        cms.PSet(
+            algorithm = cms.string('AdaptiveVertexFitter'),
+            chi2cutoff = cms.double(3.0),
+            label = cms.string(''),
+            maxDistanceToBeam = cms.double(1.0),
+            minNdof = cms.double(0.0),
+            useBeamConstraint = cms.bool(False)
+        ),
+        cms.PSet(
+            algorithm = cms.string('AdaptiveVertexFitter'),
+            chi2cutoff = cms.double(3.0),
+            label = cms.string('WithBS'),
+            maxDistanceToBeam = cms.double(1.0),
+            minNdof = cms.double(0.0),
+            useBeamConstraint = cms.bool(True)
+        )
+    )
+)
+
+
+hltVerticesPFSelector = cms.EDFilter("PrimaryVertexObjectFilter",
+    filterParams = cms.PSet(
+        maxRho = cms.double(2.0),
+        maxZ = cms.double(24.0),
+        minNdof = cms.double(4.0),
+        pvSrc = cms.InputTag("hltVerticesPF")
+        # pvSrc = cms.InputTag("firstStepPrimaryVertices")
+    ),
+    src = cms.InputTag("hltVerticesPF")
+    # src = cms.InputTag("firstStepPrimaryVertices")
+)
+
+
+hltVerticesPFFilter = cms.EDFilter("VertexSelector",
+    cut = cms.string('!isFake'),
+    filter = cms.bool(True),
+    src = cms.InputTag("hltVerticesPFSelector")
+)
+
+hltPFJetForBtagSelector = cms.EDFilter("HLT1PFJet",
+    MaxEta = cms.double(2.6),
+    MaxMass = cms.double(-1.0),
+    MinE = cms.double(-1.0),
+    MinEta = cms.double(-1.0),
+    MinMass = cms.double(-1.0),
+    MinN = cms.int32(1),
+    MinPt = cms.double(30.0),
+    # inputTag = cms.InputTag("hltAK4PFJetsCorrected"),
+    inputTag = cms.InputTag("ak4PFJetsCHSCorrected"),
+    saveTags = cms.bool(True),
+    triggerType = cms.int32(86)
+)
+
+hltPFJetForBtag = cms.EDProducer("HLTPFJetCollectionProducer",
+    HLTObject = cms.InputTag("hltPFJetForBtagSelector"),
+    TriggerTypes = cms.vint32(86)
+)
+
+hltDeepBLifetimeTagInfosPF = cms.EDProducer("CandIPProducer",
+    # candidates = cms.InputTag("hltParticleFlow"),
+    candidates = cms.InputTag("particleFlowTmp"),
+    computeGhostTrack = cms.bool(True),
+    computeProbabilities = cms.bool(True),
+    ghostTrackPriorDeltaR = cms.double(0.03),
+    jetDirectionUsingGhostTrack = cms.bool(False),
+    jetDirectionUsingTracks = cms.bool(False),
+    jets = cms.InputTag("hltPFJetForBtag"),
+    maxDeltaR = cms.double(0.4),
+    maximumChiSquared = cms.double(5.0),
+    maximumLongitudinalImpactParameter = cms.double(17.0),
+    maximumTransverseImpactParameter = cms.double(0.2),
+    minimumNumberOfHits = cms.int32(3),
+    minimumNumberOfPixelHits = cms.int32(2),
+    minimumTransverseMomentum = cms.double(1.0),
+    primaryVertex = cms.InputTag("hltVerticesPFFilter"),
+    useTrackQuality = cms.bool(False)
+)
+
+
+hltDeepInclusiveVertexFinderPF = cms.EDProducer("InclusiveCandidateVertexFinder",
+    beamSpot = cms.InputTag("hltOnlineBeamSpot"),
+    clusterizer = cms.PSet(
+        clusterMaxDistance = cms.double(0.05),
+        clusterMaxSignificance = cms.double(4.5),
+        clusterMinAngleCosine = cms.double(0.5),
+        distanceRatio = cms.double(20.0),
+        seedMax3DIPSignificance = cms.double(9999.0),
+        seedMax3DIPValue = cms.double(9999.0),
+        seedMin3DIPSignificance = cms.double(1.2),
+        seedMin3DIPValue = cms.double(0.005)
+    ),
+    fitterRatio = cms.double(0.25),
+    fitterSigmacut = cms.double(3.0),
+    fitterTini = cms.double(256.0),
+    maxNTracks = cms.uint32(30),
+    maximumLongitudinalImpactParameter = cms.double(0.3),
+    maximumTimeSignificance = cms.double(3.0),
+    minHits = cms.uint32(8),
+    minPt = cms.double(0.8),
+    primaryVertices = cms.InputTag("hltVerticesPFFilter"),
+    # tracks = cms.InputTag("hltParticleFlow"),
+    tracks = cms.InputTag("particleFlowTmp"),
+    useDirectVertexFitter = cms.bool(True),
+    useVertexReco = cms.bool(True),
+    vertexMinAngleCosine = cms.double(0.95),
+    vertexMinDLen2DSig = cms.double(2.5),
+    vertexMinDLenSig = cms.double(0.5),
+    vertexReco = cms.PSet(
+        finder = cms.string('avr'),
+        primcut = cms.double(1.0),
+        seccut = cms.double(3.0),
+        smoothing = cms.bool(True)
+    )
+)
+
+
+hltDeepInclusiveSecondaryVerticesPF = cms.EDProducer("CandidateVertexMerger",
+    maxFraction = cms.double(0.7),
+    minSignificance = cms.double(2.0),
+    secondaryVertices = cms.InputTag("hltDeepInclusiveVertexFinderPF")
+)
+
+
+
+
+hltDeepTrackVertexArbitratorPF = cms.EDProducer("CandidateVertexArbitrator",
+    beamSpot = cms.InputTag("hltOnlineBeamSpot"),
+    dLenFraction = cms.double(0.333),
+    dRCut = cms.double(0.4),
+    distCut = cms.double(0.04),
+    fitterRatio = cms.double(0.25),
+    fitterSigmacut = cms.double(3.0),
+    fitterTini = cms.double(256.0),
+    maxTimeSignificance = cms.double(3.5),
+    primaryVertices = cms.InputTag("hltVerticesPFFilter"),
+    secondaryVertices = cms.InputTag("hltDeepInclusiveSecondaryVerticesPF"),
+    sigCut = cms.double(5.0),
+    trackMinLayers = cms.int32(4),
+    trackMinPixels = cms.int32(1),
+    trackMinPt = cms.double(0.4),
+    # tracks = cms.InputTag("hltParticleFlow")
+    tracks = cms.InputTag("particleFlowTmp")
+)
+
+
+
+hltDeepInclusiveMergedVerticesPF = cms.EDProducer("CandidateVertexMerger",
+    maxFraction = cms.double(0.2),
+    minSignificance = cms.double(10.0),
+    secondaryVertices = cms.InputTag("hltDeepTrackVertexArbitratorPF")
+)
+
+
+
+
+hltDeepSecondaryVertexTagInfosPF = cms.EDProducer("CandSecondaryVertexProducer",
+    beamSpotTag = cms.InputTag("hltOnlineBeamSpot"),
+    constraint = cms.string('BeamSpot'),
+    extSVCollection = cms.InputTag("hltDeepInclusiveMergedVerticesPF"),
+    extSVDeltaRToJet = cms.double(0.3),
+    minimumTrackWeight = cms.double(0.5),
+    trackIPTagInfos = cms.InputTag("hltDeepBLifetimeTagInfosPF"),
+    trackSelection = cms.PSet(
+        a_dR = cms.double(-0.001053),
+        a_pT = cms.double(0.005263),
+        b_dR = cms.double(0.6263),
+        b_pT = cms.double(0.3684),
+        jetDeltaRMax = cms.double(0.3),
+        maxDecayLen = cms.double(99999.9),
+        maxDistToAxis = cms.double(0.2),
+        max_pT = cms.double(500.0),
+        max_pT_dRcut = cms.double(0.1),
+        max_pT_trackPTcut = cms.double(3.0),
+        min_pT = cms.double(120.0),
+        min_pT_dRcut = cms.double(0.5),
+        normChi2Max = cms.double(99999.9),
+        pixelHitsMin = cms.uint32(2),
+        ptMin = cms.double(1.0),
+        qualityClass = cms.string('any'),
+        sip2dSigMax = cms.double(99999.9),
+        sip2dSigMin = cms.double(-99999.9),
+        sip2dValMax = cms.double(99999.9),
+        sip2dValMin = cms.double(-99999.9),
+        sip3dSigMax = cms.double(99999.9),
+        sip3dSigMin = cms.double(-99999.9),
+        sip3dValMax = cms.double(99999.9),
+        sip3dValMin = cms.double(-99999.9),
+        totalHitsMin = cms.uint32(3),
+        useVariableJTA = cms.bool(False)
+    ),
+    trackSort = cms.string('sip3dSig'),
+    useExternalSV = cms.bool(True),
+    usePVError = cms.bool(True),
+    vertexCuts = cms.PSet(
+        distSig2dMax = cms.double(99999.9),
+        distSig2dMin = cms.double(2.0),
+        distSig3dMax = cms.double(99999.9),
+        distSig3dMin = cms.double(-99999.9),
+        distVal2dMax = cms.double(2.5),
+        distVal2dMin = cms.double(0.01),
+        distVal3dMax = cms.double(99999.9),
+        distVal3dMin = cms.double(-99999.9),
+        fracPV = cms.double(0.79),
+        massMax = cms.double(6.5),
+        maxDeltaRToJetAxis = cms.double(0.4),
+        minimumTrackWeight = cms.double(0.5),
+        multiplicityMin = cms.uint32(2),
+        useTrackWeights = cms.bool(True),
+        v0Filter = cms.PSet(
+            k0sMassWindow = cms.double(0.05)
+        )
+    ),
+    vertexReco = cms.PSet(
+        finder = cms.string('avr'),
+        minweight = cms.double(0.5),
+        primcut = cms.double(1.8),
+        seccut = cms.double(6.0),
+        smoothing = cms.bool(False),
+        weightthreshold = cms.double(0.001)
+    ),
+    vertexSelection = cms.PSet(
+        sortCriterium = cms.string('dist3dError')
+    )
+)
+
+
+
+
+
+hltDeepCombinedSecondaryVertexBJetTagsInfos = cms.EDProducer("DeepNNTagInfoProducer",
+    computer = cms.PSet(
+        SoftLeptonFlip = cms.bool(False),
+        charmCut = cms.double(1.5),
+        correctVertexMass = cms.bool(True),
+        minimumTrackWeight = cms.double(0.5),
+        pseudoMultiplicityMin = cms.uint32(2),
+        pseudoVertexV0Filter = cms.PSet(
+            k0sMassWindow = cms.double(0.05)
+        ),
+        trackFlip = cms.bool(False),
+        trackMultiplicityMin = cms.uint32(2),
+        trackPairV0Filter = cms.PSet(
+            k0sMassWindow = cms.double(0.03)
+        ),
+        trackPseudoSelection = cms.PSet(
+            a_dR = cms.double(-0.001053),
+            a_pT = cms.double(0.005263),
+            b_dR = cms.double(0.6263),
+            b_pT = cms.double(0.3684),
+            jetDeltaRMax = cms.double(0.3),
+            maxDecayLen = cms.double(5.0),
+            maxDistToAxis = cms.double(0.07),
+            max_pT = cms.double(500.0),
+            max_pT_dRcut = cms.double(0.1),
+            max_pT_trackPTcut = cms.double(3.0),
+            min_pT = cms.double(120.0),
+            min_pT_dRcut = cms.double(0.5),
+            normChi2Max = cms.double(99999.9),
+            pixelHitsMin = cms.uint32(0),
+            ptMin = cms.double(0.0),
+            qualityClass = cms.string('any'),
+            sip2dSigMax = cms.double(99999.9),
+            sip2dSigMin = cms.double(2.0),
+            sip2dValMax = cms.double(99999.9),
+            sip2dValMin = cms.double(-99999.9),
+            sip3dSigMax = cms.double(99999.9),
+            sip3dSigMin = cms.double(-99999.9),
+            sip3dValMax = cms.double(99999.9),
+            sip3dValMin = cms.double(-99999.9),
+            totalHitsMin = cms.uint32(3),
+            useVariableJTA = cms.bool(False)
+        ),
+        trackSelection = cms.PSet(
+            a_dR = cms.double(-0.001053),
+            a_pT = cms.double(0.005263),
+            b_dR = cms.double(0.6263),
+            b_pT = cms.double(0.3684),
+            jetDeltaRMax = cms.double(0.3),
+            maxDecayLen = cms.double(5.0),
+            maxDistToAxis = cms.double(0.07),
+            max_pT = cms.double(500.0),
+            max_pT_dRcut = cms.double(0.1),
+            max_pT_trackPTcut = cms.double(3.0),
+            min_pT = cms.double(120.0),
+            min_pT_dRcut = cms.double(0.5),
+            normChi2Max = cms.double(99999.9),
+            pixelHitsMin = cms.uint32(2),
+            ptMin = cms.double(0.0),
+            qualityClass = cms.string('any'),
+            sip2dSigMax = cms.double(99999.9),
+            sip2dSigMin = cms.double(-99999.9),
+            sip2dValMax = cms.double(99999.9),
+            sip2dValMin = cms.double(-99999.9),
+            sip3dSigMax = cms.double(99999.9),
+            sip3dSigMin = cms.double(-99999.9),
+            sip3dValMax = cms.double(99999.9),
+            sip3dValMin = cms.double(-99999.9),
+            totalHitsMin = cms.uint32(3),
+            useVariableJTA = cms.bool(False)
+        ),
+        trackSort = cms.string('sip2dSig'),
+        useTrackWeights = cms.bool(True),
+        vertexFlip = cms.bool(False)
+    ),
+    svTagInfos = cms.InputTag("hltDeepSecondaryVertexTagInfosPF")
+    # svTagInfos = cms.InputTag("inclusiveSecondaryVertices")
+)
+
+
+
+
+hltDeepCombinedSecondaryVertexBJetTagsPF = cms.EDProducer("DeepFlavourJetTagsProducer",
+    NNConfig = cms.FileInPath('RecoBTag/Combined/data/DeepCSV_PhaseI.json'),
+    checkSVForDefaults = cms.bool(True),
+    meanPadding = cms.bool(True),
+    src = cms.InputTag("hltDeepCombinedSecondaryVertexBJetTagsInfos"),
+    toAdd = cms.PSet(
+        probbb = cms.string('probb')
+    )
+)
+
+
+HLTBtagDeepCSVSequencePF = cms.Sequence(
+    hltVerticesPF
+	+hltVerticesPFSelector
+	+hltVerticesPFFilter
+	+hltPFJetForBtagSelector
+	+hltPFJetForBtag
+	+hltDeepBLifetimeTagInfosPF
+	+hltDeepInclusiveVertexFinderPF
+	+hltDeepInclusiveSecondaryVerticesPF
+	+hltDeepTrackVertexArbitratorPF
+	+hltDeepInclusiveMergedVerticesPF
+	+hltDeepSecondaryVertexTagInfosPF
+	+hltDeepCombinedSecondaryVertexBJetTagsInfos
+	+hltDeepCombinedSecondaryVertexBJetTagsPF)
