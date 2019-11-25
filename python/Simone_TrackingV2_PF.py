86749538cfaba16647690ee2f03e04dea4e2d650
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2019-11-25 10:09:47 +0100

    disable multiThreading

--- python/Simone_TrackingV2_PF.py
+++ python/Simone_TrackingV2_PF.py
@@ -546,7 +546,8 @@ process.HLTOutput = cms.EndPath( process.hltOutput )
 
 # load the DQMStore and DQMRootOutputModule
 process.load( "DQMServices.Core.DQMStore_cfi" )
-process.DQMStore.enableMultiThread = True
+# process.DQMStore.enableMultiThread = True
+process.DQMStore.enableMultiThread = False
 
 
 # configure the FastTimerService
@@ -595,8 +596,10 @@ process.FastTimerService.enableDQMbyProcesses      = False
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool( True )
 )
-process.options.numberOfStreams = cms.untracked.uint32(4)
-process.options.numberOfThreads = cms.untracked.uint32(4)
+# process.options.numberOfStreams = cms.untracked.uint32(4)
+# process.options.numberOfThreads = cms.untracked.uint32(4)
+process.options.numberOfStreams = cms.untracked.uint32(1)
+process.options.numberOfThreads = cms.untracked.uint32(1)
 
 
 # FastTimerService client
