608361531374c75a2821a857b8eaf40976ca9551
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2019-12-09 14:32:27 +0100

    switch to PhaseII training for trackingV0

--- python/BTagHLT_stripped_cff.py
+++ python/BTagHLT_stripped_cff.py
@@ -347,7 +347,8 @@ hltDeepCombinedSecondaryVertexBJetTagsInfos = cms.EDProducer("DeepNNTagInfoProdu
 
 
 hltDeepCombinedSecondaryVertexBJetTagsPF = cms.EDProducer("DeepFlavourJetTagsProducer",
-    NNConfig = cms.FileInPath('RecoBTag/Combined/data/DeepCSV_PhaseI.json'),
+    # NNConfig = cms.FileInPath('RecoBTag/Combined/data/DeepCSV_PhaseI.json'),
+    NNConfig = cms.FileInPath('RecoBTag/Combined/data/DeepCSV_PhaseII.json'),
     checkSVForDefaults = cms.bool(True),
     meanPadding = cms.bool(True),
     src = cms.InputTag("hltDeepCombinedSecondaryVertexBJetTagsInfos"),
