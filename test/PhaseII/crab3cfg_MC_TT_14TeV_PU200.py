d4c3e8438b766723b34222b11a5225c29409a46e
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2020-01-09 12:47:59 +0100

    update crab TT config

--- test/PhaseII/crab3cfg_MC_TT_14TeV_PU200.py
+++ test/PhaseII/crab3cfg_MC_TT_14TeV_PU200.py
@@ -1,6 +1,6 @@
 from WMCore.Configuration import Configuration
 
-store_dir = 'BTagServiceWork/PhaseII/Online/TrackingV0'
+store_dir = 'BTagServiceWork/PhaseII/Online/NewGT_TrackingV2_1'
 sample_name = 'TT_14TeV_PU200'
 
 # MIN_DSET = '/TT_TuneCP5_14TeV-powheg-pythia8/PhaseIITDRSpring19MiniAOD-PU200_106X_upgrade2023_realistic_v3-v1/MINIAODSIM'
@@ -9,7 +9,7 @@ RAW_DSET = '/TT_TuneCP5_14TeV-powheg-pythia8/PhaseIITDRSpring19DR-PU200_106X_upg
 config = Configuration()
 
 config.section_('General')
-config.General.requestName = 'bTagHLTAnalyzer_'+sample_name
+config.General.requestName = 'bTagHLTAnalyzer_'+sample_name+"_NewGT_TrackingV2_1"
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
@@ -17,25 +17,27 @@ config.section_('JobType')
 config.JobType.pluginName  = 'Analysis'
 config.JobType.psetName = 'runHLTBTagAnalyzer_PhaseII_cfg.py'
 config.JobType.inputFiles = []
-config.JobType.pyCfgParams = ['output='+sample_name+'.root']
+#~ config.JobType.pyCfgParams = ['output='+sample_name+'.root']
+config.JobType.pyCfgParams = []
 config.JobType.maxJobRuntimeMin = 2750
-config.JobType.maxMemoryMB = 4000
-#config.JobType.numCores = 4
+config.JobType.maxMemoryMB = 3500
+#config.JobType.numCores = 1
 
 config.section_('Data')
 config.Data.publication = False
 config.Data.ignoreLocality = True
-config.Data.splitting = 'FileBased'
+config.Data.splitting = 'EventAwareLumiBased'
 # config.Data.inputDataset = MIN_DSET
 config.Data.inputDataset = RAW_DSET
 # config.Data.secondaryInputDataset = RAW_DSET
 config.Data.outLFNDirBase = '/store/user/sewuchte/'+store_dir+'/'+sample_name
-config.Data.unitsPerJob = 1
+config.Data.unitsPerJob = 40
 config.Data.totalUnits = -1
 
 config.section_('Site')
 config.Site.storageSite = 'T2_DE_DESY'
 if config.Data.ignoreLocality:
-   config.Site.whitelist = ['T2_CH_CERN', 'T2_DE_*']
+   #~ config.Site.whitelist = ['T2_CH_CERN', 'T2_DE_*']
+   config.Site.whitelist = ['T2_DE_*','T1_DE_*']
 
 config.section_('User')
