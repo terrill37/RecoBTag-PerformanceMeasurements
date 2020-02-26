57ecf8c15fbf6c8dc0e093e5e552924867a2f84f
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2020-02-26 14:45:58 +0100

    new eta cuts, track histograms, and skimmed tracks

--- test/PhaseII/runHLTBTagAnalyzer_PhaseII_cfg.py
+++ test/PhaseII/runHLTBTagAnalyzer_PhaseII_cfg.py
@@ -50,22 +50,34 @@ opts.register('htrk', False,
               vpo.VarParsing.varType.bool,
               'added monitoring histograms for selected Tracks and Vertices')
 
+opts.register('skimTracks', False,
+              vpo.VarParsing.multiplicity.singleton,
+              vpo.VarParsing.varType.bool,
+              'skim original collection of generalTracks (only tracks associated to first N pixel vertices)')
+
 opts.parseArguments()
 
 if opts.doTrackV0:
-    # use the following two lines for tracking V0 setup
-    from RecoBTag.PerformanceMeasurements.step3_TrackingV0_11_0_0 import cms, process
-    process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_cff")
+    if opts.runCMSSW11Sample:
+        # use the following two lines for tracking V2 setup
+        from RecoBTag.PerformanceMeasurements.step3_TrackingV0_11_0_0_samples import cms, process
+        # process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_cff")
+    else:
+        # use the following two lines for tracking V0 setup
+        from RecoBTag.PerformanceMeasurements.step3_TrackingV0_11_0_0 import cms, process
+        # process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_cff")
 else:
     if opts.runCMSSW11Sample:
         # use the following two lines for tracking V2 setup
         from RecoBTag.PerformanceMeasurements.step3_TrackingV2_11_0_0_samples import cms, process
-        process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_trackV2_cff")
+        # process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_trackV2_cff")
     else:
         # use the following two lines for tracking V2 setup
         from RecoBTag.PerformanceMeasurements.step3_TrackingV2_11_0_0 import cms, process
-        process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_trackV2_cff")
+        # process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_trackV2_cff")
 
+# process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_trackV2_cff")
+process.load("RecoBTag.PerformanceMeasurements.BTagHLT_stripped_trackV2_LikeOffline11_cff")
 
 # remove cms.EndPath for EDM output
 del process.HLTOutput
@@ -156,6 +168,74 @@ process.reconstruction *= process.ak4CaloJetsSeq
 #process.L1Reco_step = cms.Path(process.L1Reco)
 # process.reconstruction_step = cms.Path(process.reconstruction)
 
+
+
+# skimming of tracks
+if opts.skimTracks:
+
+   ###
+   ### redefine GeneralTracks, selecting a subset of tracks associated to N pixel vertices
+   ###  - each track is associated to the pixel vertex which is closest to it in Z
+   ###  - the track is retained only if the associated pixel vertex is one of the first N of the vertex collection (ranking is based on sum-pT2)
+   ###
+   # clone original collection of generalTracks
+   process.generalTracksOriginal = process.generalTracks.clone()
+
+   # re-order (see ranker) and restrict the original list of pixel vertices similarly
+   # to what was done at HLT in Run-2 (see hltTrimmedPrimaryVertices in 2018 HLT Menu)
+   process.hltTrimmedPixelVertices = cms.EDProducer('PixelVerticesSelector',
+
+     src = cms.InputTag('pixelVertices'),
+
+     minSumPt2 = cms.double( 0.0 ),
+     minSumPt2FractionWrtMax = cms.double( 0.3 ),
+
+     # criterion to rank pixel vertices
+     # (utilizes PVClusterComparer to compute
+     # the vertex SumPtSquared f.o.m. using a sub-set of tracks)
+     ranker = cms.PSet(
+       track_chi2_max = cms.double( 20.0 ),
+       track_pt_max = cms.double( 20.0 ),
+       track_prob_min = cms.double( -1.0 ),
+       track_pt_min = cms.double( 1.0 )
+     ),
+
+     # retain only first N vertices
+     maxNVertices = cms.int32( -1 ),
+   )
+
+   # updated collection of generalTracks
+   #  - redefine the module "generalTracks", so that downstream modules
+   #    automatically use this updated collection
+   #    (instead of the original "generalTracks" collection)
+   #  - new set of generalTracks contains only the input tracks
+   #    associated to one of the first N pixel vertices
+   process.generalTracks = cms.EDProducer('TracksClosestToFirstVerticesSelector',
+
+     tracks = cms.InputTag('generalTracksOriginal'),
+     vertices = cms.InputTag('hltTrimmedPixelVertices'),
+
+     # retain only tracks associated to one of the first N vertices
+     maxNVertices = cms.int32( 10 ),
+
+     # track-vertex association: max delta-Z between track and z-closest vertex
+     maxDeltaZ = cms.double( 0.2 ),
+   )
+
+   # insert updated generalTracks into tracking sequence and related task
+   process.globalreco_tracking.replace(process.generalTracks, cms.Sequence(
+      (process.generalTracksOriginal
+     +(process.reconstruction_pixelTrackingOnly * process.hltTrimmedPixelVertices))
+     * process.generalTracks
+   ))
+
+   process.generalTracksTask.add(process.generalTracksOriginal, process.hltTrimmedPixelVertices)
+
+
+
+
+
+
 process.noFilter_PFDeepCSV = cms.Path(process.HLTBtagDeepCSVSequencePF)
 
 
@@ -245,8 +325,10 @@ if opts.inputFiles:
 else:
    process.source.fileNames = [
      # "file:/eos/cms/store/mc/PhaseIITDRSpring19DR/TTbar_14TeV_TuneCP5_Pythia8/GEN-SIM-DIGI-RAW/PU200_106X_upgrade2023_realistic_v3_ext1-v3/60000/A7DE6079-B3AE-4743-A5F3-2050EDEB8383.root",
+     # "/store/relval/CMSSW_11_0_0/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/110X_mcRun4_realistic_v2_2026D49noPU-v2/20000/26BAF436-18BA-4843-95E6-100394440248.root",
+     "/store/relval/CMSSW_11_0_0/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU25ns_110X_mcRun4_realistic_v3_2026D49PU200-v1/20000/F4CA9166-52A2-474F-8D30-9B70B4B28361.root",
      # /RelValTTbar_14TeV/CMSSW_11_0_0_pre13-110X_mcRun4_realistic_v2_2026D49noPU-v1/GEN-SIM-DIGI-RAW",
-     "/store/relval/CMSSW_11_0_0_pre13/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/110X_mcRun4_realistic_v2_2026D49noPU-v1/20000/D7F3470F-864E-944B-922E-7178756F4652.root",
+     # "/store/relval/CMSSW_11_0_0_pre13/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/110X_mcRun4_realistic_v2_2026D49noPU-v1/20000/D7F3470F-864E-944B-922E-7178756F4652.root",
    ]
 
 
@@ -354,11 +436,13 @@ for requiredGroup in process.btagana.groups:
      if (requiredGroup.group == storedGroup.group):
        requiredGroup.store = storedGroup.store
 
-process.btagana.MaxEta                = 2.4
+# process.btagana.MaxEta                = 2.4
+process.btagana.MaxEta                = 4.5
 process.btagana.MinPt                 = 30
 # process.btagana.triggerTable          = cms.InputTag('TriggerResults::HLT') # Data and MC
 process.btagana.triggerTable          = cms.InputTag('TriggerResults::RECO2') # Data and MC
-process.btagana.primaryVertexColl     = cms.InputTag('hltVerticesPF')
+# process.btagana.primaryVertexColl     = cms.InputTag('hltVerticesPF')
+process.btagana.primaryVertexColl     = cms.InputTag('offlinePrimaryVertices') #change with new Offline like sequence
 # process.btagana.primaryVertexColl     = cms.InputTag('firstStepPrimaryVertices')
 
 process.btagana.runHLTJetVariables     = cms.bool(True)
@@ -380,10 +464,31 @@ process.allEvents = eventCounter.clone()
 process.selectedEvents = eventCounter.clone()
 #---------------------------------------
 # Tracking Monitoring
+# if opts.htrk:
+#     from RecoBTag.PerformanceMeasurements.myTrackAnalyzer_cfi import myTrackAnalyzer
+#     process.trackAnalyzer = myTrackAnalyzer.clone()
+#     process.trackAnalyzer.tracks = cms.InputTag('generalTracks')
+
+# Tracking Monitoring
 if opts.htrk:
-    from RecoBTag.PerformanceMeasurements.myTrackAnalyzer_cfi import myTrackAnalyzer
-    process.trackAnalyzer = myTrackAnalyzer.clone()
-    process.trackAnalyzer.tracks = cms.InputTag('generalTracks')
+   process.reconstruction_pixelTrackingOnly_step = cms.Path(process.reconstruction_pixelTrackingOnly)
+
+   from JMETriggerAnalysis.Common.TrackHistogrammer_cfi import TrackHistogrammer
+   process.TrackHistograms_pixelTracks = TrackHistogrammer.clone(src = cms.InputTag('pixelTracks'))
+   process.TrackHistograms_generalTracks = TrackHistogrammer.clone(src = cms.InputTag('generalTracks'))
+
+   from JMETriggerAnalysis.Common.VertexHistogrammer_cfi import VertexHistogrammer
+   process.VertexHistograms_pixelVertices = VertexHistogrammer.clone(src = cms.InputTag('pixelVertices'))
+   process.VertexHistograms_offlinePrimaryVertices = VertexHistogrammer.clone(src = cms.InputTag('offlinePrimaryVertices'))
+
+   process.trkMonitoringSeq = cms.Sequence(
+       process.TrackHistograms_pixelTracks
+     + process.TrackHistograms_generalTracks
+     + process.VertexHistograms_pixelVertices
+     + process.VertexHistograms_offlinePrimaryVertices
+   )
+   process.trkMonitoringEndPath = cms.EndPath(process.trkMonitoringSeq)
+
 
 ## Define analyzer sequence
 process.analyzerSeq = cms.Sequence(process.btagana)
@@ -397,8 +502,8 @@ process.p = cms.Path(
     # * process.analyzerSeq
     # * process.trackAnalyzer
 )
-if opts.htrk:
-    process.p*=process.trackAnalyzer
+# if opts.htrk:
+#     process.p*=process.trackAnalyzer
 
 process.analysisNTupleEndPath = cms.EndPath(process.analyzerSeq)
 
@@ -419,4 +524,5 @@ print 'dumpPython =', opts.dumpPython
 print 'doTrackHistos =', opts.htrk
 print 'doTrackingV0 =', opts.doTrackV0
 print 'runCMSSW11Sample =', opts.runCMSSW11Sample
+print 'useSkimmedTracks =', opts.skimTracks
 print '\n-------------------------------'
