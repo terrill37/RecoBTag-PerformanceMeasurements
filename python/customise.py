5345a96b901d389e099c75495d66c7398cd2af44
Wesley Terrill <terrill.37@osu.edu>
2020-07-09 21:13:32 +0200

    removed unused function associated with trigger change

--- python/customise.py
+++ python/customise.py
@@ -225,25 +225,4 @@ def customize_CaloJet(process,ptVal=0.9): #FIXME
   return process
 
 
-#def bTagHLTAnalyzer_custom():
-#
-#bTagHLTAnalyzer = cms.EDAnalyzer("BTagHLTAnalyzer",
-#                                  bTagAnalyzerCommon,
-#                                  variableSet,
-#                                  groupSet,
-#                                  HLTprimaryVertexColl = cms.InputTag('hltVerticesPFFilter'),
-#                                  CaloJets             = cms.InputTag('hltAK4CaloJetsCorrectedIDPassed'),
-#                                  CaloJetTags          = cms.InputTag('hltDeepCombinedSecondaryVertexBJetTagsInfosCalo'),
-#                                  CaloSVs              = cms.InputTag('hltInclusiveSecondaryVertexFinderTagInfos'),
-#                                  CaloJetCSVTags       = cms.InputTag('hltCombinedSecondaryVertexBJetTagsCalo'),
-#                                  CaloJetDeepCSVTags   = cms.InputTag('hltDeepCombinedSecondaryVertexBJetTagsCalo:probb'),
-#                                  PFJets               = cms.InputTag('hltAK4PFJetsLooseIDCorrected'),
-#                                  PFJetTags            = cms.InputTag('hltDeepCombinedSecondaryVertexBJetTagsInfos'),
-#                                  PFSVs                = cms.InputTag('hltDeepSecondaryVertexTagInfosPF'),
-#                                  PFJetCSVTags         = cms.InputTag('hltCombinedSecondaryVertexBJetTagsPF'), 
-#                                  PFJetDeepCSVTags     = cms.InputTag('hltDeepCombinedSecondaryVertexBJetTagsPF:probb'),
-#                                  HLTTriggerPathNames  = cms.vstring("HLT_ZeroBias_Beamspot_v*"),
-#                                                                    # "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_Is
-#                                                                    # "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_*"), #edit here FIXME              
-#)
-#                                                                                                    
+                                                                                                    
